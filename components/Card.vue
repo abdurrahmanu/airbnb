ffec4dca44b0f55e0d5fc9c70696ae1c409c3e52
Ahmed Abdulrahman <104634941+abdurrahmanu@users.noreply.github.com>
2024-02-15 08:04:47 +0100

    Update Card.vue

--- components/Card.vue
+++ components/Card.vue
@@ -4,6 +4,7 @@
             <img class="rounded-[13px]" height="120%" width="100%"  src="/img.jpg" alt="">
             <div class="absolute top-3 right-3">like</div>
             <div class="absolute top-3 left-3">guest tag</div>
+           <div v-for="(img, index) in imgs" :key="index" :class=[index === 2 ? "bg-blue": ""] :style="{height: index + 1 + 'px'; width: index + 1 + 'px'}" class="rounded-full absolute bottom-3 left-[50%] translate-x-[-50%]"></div>
         </div>
         <div class="space-y-1">
             <div class="flex justify-between">
@@ -18,7 +19,7 @@
 </template>
 
 <script setup>
-
+const imgs = 5
 </script>
 
 <style lang="scss" scoped>
